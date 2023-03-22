087fb24e3d0b29e7e6d8b29984c2bb2324e8766c
Michael Blaydes <mcblaydesconsulting@gmail.com>
2023-03-22 16:29:35 -0400

    Add project files.

---
+++ Debt Calculator/README.md
@@ -0,0 +1,3 @@
+# Debt Calculator
+
+
---
+++ Debt Calculator/script.js
@@ -0,0 +1,9 @@
+let totDebt = document.getElementById("totDebt");
+let intRate = document.getElementById("intRate");
+let numMonths = document.getElementById("numMonths");
+let payments = document.getElementById("payments");
+
+const submit = document.getElementById("submitInfo");
+submit.addEventListener("click", function () {
+    console.log(totDebt);
+});
