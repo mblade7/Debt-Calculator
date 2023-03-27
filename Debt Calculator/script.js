76be2d5b5d625ee10eb0c71465800556d9c84fa5
Michael Blaydes <mcblaydesconsulting@gmail.com>
2023-03-27 13:42:02 -0400

    Trying to add functionality

--- Debt Calculator/script.js
+++ Debt Calculator/script.js
@@ -3,10 +3,20 @@ calculate.addEventListener("submit", function() {
     event.preventDefault();
     let totDebt = document.getElementById("totDebt").value;
     let intRate = document.getElementById("intRate").value;
-    let numMonths = document.getElementById("numMonths").value;
+    //let numMonths = document.getElementById("numMonths").value;
     let payments = document.getElementById("payments").value;
+    let numPayments;
     console.log(totDebt);
     console.log(intRate);
-    console.log(numMonths);
     console.log(payments);
+    if (totDebt > 0) {
+        let numPayments = 1;
+        do {
+            let newDebt = totDebt + (totDebt * (intRate / 100 / 12) - payments);
+            numPayments++;
+            console.log(numPayments);
+            console.log(newDebt);
+        }
+        while (newDebt > 0);
+    }
 });
